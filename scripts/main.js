cd48884e5c0f59f5b3edff4e009a49bbc7af3f8f
Denis <smithson11001@gmail.com>
2022-12-31 16:45:19 +0300

    * Добавил плавный переход

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,9 @@
 let message = checkString(prompt('Как вас зовут?', `Алексей`))
 
 let nameIntro = document.getElementById('name_intro');
+let body = document.body;
 nameIntro.textContent = `Здравствуйте, ${message ?? 'Гость'}!`;
+body.style.opacity = '100%'
 
 function checkString(string) {
     if (string === '') return 'Гость';
