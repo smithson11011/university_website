6396a12a4a1039fc8239a46319fb1a938967b7fd
Denis <smithson11001@gmail.com>
2023-01-02 08:57:05 +0300

    * Добавил кнопку "Скрыть меню"

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,16 @@
 
 let message = checkString(prompt('Как вас зовут?', `Алексей`))
 
+let hideMenuButton = document.getElementById('hideMenu');
 let nameIntro = document.getElementById('name_intro');
+let aboutButton = document.getElementById('about_uni_button');
+let buttons = document.getElementsByClassName('buttons_menu');
 let body = document.body;
+let isShown = true;
 nameIntro.textContent = `Здравствуйте, ${message ?? 'Гость'}!`;
-body.style.opacity = '100%'
+body.style.opacity = '100%';
+hideMenuButton.style.marginLeft = '400px';
+
 
 function checkString(string) {
     if (string === '') return 'Гость';
